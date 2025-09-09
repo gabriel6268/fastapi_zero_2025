dd213ce33000ff42ed90cdf17a7661f88a702f8d
F3544841 Gabriel Gomes Da Silva <gomesgabriel@bb.com.br>
2025-09-09 17:30:55 -0300

    Adicionado endpoint de estudos HTML

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get('/')
+# Endpoint inicial para exemplo
+@app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Olá, Mundo!'}
+
+
+# Endpoint para exemplo de html
+@app.get('/hello/', response_class=HTMLResponse, status_code=HTTPStatus.OK)
+def hello():
+    return """
+        <html>
+            <head>
+                <title>Hello, World!</title>
+            </head>
+            <body>
+                <h1>Hello, World!!!</h1>
+            </body>
+        </html>
+    """
